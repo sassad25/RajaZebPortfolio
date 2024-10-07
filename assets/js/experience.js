3baab3537eb8a87822ef3ab18bceac7ca9ab08fa
Raja Zeb <zeblearns3@gmail.com>
2024-10-07 14:12:52 -0400

    Experience changes test

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,25 +5,25 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
+    title: "Sr. Software Engineer",
     cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    place: "Fiserv, Berkeley Heights, NJ",
+    time: "(Nov, 2022 - present)",
+    desp: "<li>Developed service using Spring Cloud Function which process data sent Amazon S3 bucket and Amazon Kinesis streams using inbound Lambda which parses data, validates, calculates and stores in PostgreSQL RDS. Outbound Lambda processing generate out files sent to Legacy system.</li><li>Integrated with NACHA processors for sending and receiving ACH files.</li> <li>Refactored Vue 2 components to align with the Vue 3 component structure and composition API, improving code maintainability and readability.</li>",
   },
   {
-    title: "Student Developer",
+    title: "Sr. Software Engineer",
     cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    place: "Orbcomm, Rochelle Park, NJ",
+    time: "(Feb, 2022 - Oct, 2022)",
+    desp: "<li>Implemented UI using Angular JS, Java Script, jQuery, HTML5 &amp; CSS3.</li><li>Involved in front-end designing using HTML, CSS and JavaScript with Ajax and tag libraries.</li><li>Implemented Spring boot micro services to process the messages into the Kafka cluster setup.</li>",
   },
   {
-    title: "Research Intern",
+    title: "Sr. Software Engineer",
     cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    place: "General Motors, Detroit, MI",
+    time: "(Mar, 2019 - Jan, 2022)",
+    desp: "<li>Involved in various phases of Software Development Life Cycle (SDLC) such as requirements gathering, modeling, analysis, design, development, and testing.</li><li>Knowledge of Scala&#39;s rich collection libraries and their functional programming aspects, enabling efficient data processing and manipulation.</li>",
   },
 ];
 
